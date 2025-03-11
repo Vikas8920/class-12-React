6a6708b9da05d4b3408d9969da40f4482f01e32e
Vikas8920 <vikasmishra0306@gmail.com>
2025-03-11 12:58:41 +0530

    11-03-2025

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import './App.css';
-import Button from './Button';
+// import React from 'react'
+// import './heading.css'
+// import HelloWorld from './Component/HelloWorld'
 
-function App() {
-  return (
-  <div>
-     <Button name={'Submit'}/>
-     <Button name={'Click'}/>
-  </div>
-  );
+// const App = () => {
+//   const x = 5
+//   // let text = 'Good bye'
+
+//   // if(x<10){
+//   //   text = 'Hello React'
+//   // }
+//   return (
+//     <>
+//       <h1 className='heading'>React is {5 + 5} times better with JSX</h1>
+//       <h1>Hello</h1>
+//      <img src='' alt='name'/>
+//      <br/>
+//      <hr/>
+//      <h1>{(x)<10?'Hello React':'Good bye'}</h1>
+//      <hr/>
+//      <HelloWorld name='Vikas'/>
+//      <HelloWorld name='Aditya'/>
+     
+//     </>
+//   )
+// }
+
+// export default App
+
+import React, { Component } from 'react'
+import Greeting from './Component/Greeting'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Greeting name='Vikas'/>
+      </>
+    )
+  }
 }
 
-export default App;
---
+++ src/Component/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = ({name}) => {
+  return (
+    <>
+      <h1>Hello, {name}</h1>
+      <p>Welcome to our react application</p>
+    </>
+  )
+}
+
+export default HelloWorld
